1741253f620bc27ff2bf1f40c7d3f8663fe096e6
Max Ward <maxward3@outlook.com>
2023-11-19 12:23:24 +0000

    1

--- client/src/components/login.jsx
+++ client/src/components/login.jsx
@@ -8,15 +8,16 @@ export default function Login() {
             <div className="flex justify-center items-center h-full">
                 <form className="max-w-[400px] w-full mx-auto bg-white p-8">
                     <h2 className="text-4xl font-bold text-center py-4">MyProjects</h2>
-                    <div>
+                    <div className="flex flex-col mb-4 relative">
                         <label>Username</label>
-                        <input type="text" />
+                        <input className="border relative bg-gray-100 p-2" type="text" />
                     </div>
-                    <div>
+                    <div className="flex flex-col relative">
                         <label>Password</label>
-                        <input type="password" />
+                        <input className="border relative bg-gray-100 p-2" type="password" />
                     </div>
-                    <button>Login</button>
+                    <button className="w-full py-3 mt-8 bg-indigo-600 hover:bg-indigo-500 relative">Login</button>
+                    <p className=" flex items-center mt-2 relative"><input className="mr-2" type="checkbox" />Remember Me</p>
                 </form>
             </div>
         </div>
