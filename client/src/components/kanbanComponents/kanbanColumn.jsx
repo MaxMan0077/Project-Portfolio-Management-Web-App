3f95ddf0166451ca6938533325ed6d7ad8d8f04e
Max Ward <maxward3@outlook.com>
2024-03-17 20:26:31 +0000

    fixed header bug

--- client/src/components/kanbanComponents/kanbanColumn.jsx
+++ client/src/components/kanbanComponents/kanbanColumn.jsx
@@ -6,16 +6,18 @@ const KanbanColumn = ({ columnId, title, tasks }) => {
   return (
     <Droppable droppableId={columnId}>
       {(provided) => (
-        <div
-          {...provided.droppableProps}
-          ref={provided.innerRef}
-          className="flex flex-col bg-gray-100 p-4 rounded-md w-[calc(20%-0.5rem)] h-full overflow-auto mx-1"
-        >
+        <div className="flex flex-col bg-gray-100 p-4 rounded-md w-[calc(20%-0.5rem)] h-full overflow-auto mx-1">
           <h2 className="text-lg font-bold mb-4">{title}</h2>
-          {tasks && tasks.map((task, index) => (
-            task && <KanbanCard key={task.idproject} project={task} index={index} />
-          ))}
-          {provided.placeholder}
+          <div
+            ref={provided.innerRef}
+            {...provided.droppableProps}
+            className="flex flex-col flex-grow min-h-[2rem]" // Adjust min-h-[2rem] as needed to provide enough space
+          >
+            {tasks && tasks.map((task, index) => (
+              task && <KanbanCard key={task.idproject} project={task} index={index} />
+            ))}
+            {provided.placeholder}
+          </div>
         </div>
       )}
     </Droppable>
