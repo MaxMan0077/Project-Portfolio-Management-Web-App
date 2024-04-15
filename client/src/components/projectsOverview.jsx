48f9736383476cabdc53d324cd48dc0573603e3c
Max Ward <maxward3@outlook.com>
2024-04-15 12:45:29 +0100

    Projects table 2.0

--- client/src/components/projectsOverview.jsx
+++ client/src/components/projectsOverview.jsx
@@ -10,6 +10,9 @@ const ProjectsOverview = () => {
     const { formatMessage } = useIntl();
     const t = (id) => formatMessage({ id });
     const [projects, setProjects] = useState([]);
+    const [photos, setPhotos] = useState({});
+    const [names, setNames] = useState({});
+    const [ragStatuses, setRagStatuses] = useState({});
     const [searchTerm, setSearchTerm] = useState('');
     const [isFilterVisible, setIsFilterVisible] = useState(false);
     // Initialize state for each filter
@@ -18,21 +21,85 @@ const ProjectsOverview = () => {
     const [projectManagerFilter, setProjectManagerFilter] = useState('');
     const [businessOwnerFilter, setBusinessOwnerFilter] = useState('');
     const [programFilter, setProgramFilter] = useState('');
-
-    // Fetch Projects
-    const fetchProjects = async () => {
+    const headerColors = ['bg-blue-500', 'bg-green-500', 'bg-red-500', 'bg-yellow-500', 'bg-purple-500'];
+   
+    const fetchPhoto = async (resourceId) => {
         try {
-            const response = await axios.get('http://localhost:5001/api/projects/getall');
-            console.log('Projects Data:', response.data);
-            setProjects(response.data);
+          const response = await axios.get(`http://localhost:5001/api/resources/photo/${resourceId}`);
+          const base64Filename = response.data;
+          const decodedFilename = atob(base64Filename);
+          return `http://localhost:5001/uploads/${decodedFilename}`;
         } catch (error) {
-            console.error('Failed to fetch projects:', error);
+          console.error(`Error fetching photo for resource ID ${resourceId}:`, error);
+          return 'https://via.placeholder.com/150';
+        }
+    };
+      
+    const fetchNames = async (resourceId) => {
+        try {
+            const response = await axios.get(`http://localhost:5001/api/resources/resourceNames/${resourceId}`);
+            const { firstName, lastName } = response.data;
+            return `${firstName} ${lastName}`;
+        } catch (error) {
+            console.error(`Error fetching name for resource ID ${resourceId}:`, error);
+            return 'Unknown';
+        }
+    };
+
+    const fetchLatestRAGStatus = async (projectId) => {
+        try {
+            const response = await axios.get(`http://localhost:5001/api/reports/latestRAGStatus/${projectId}`);
+            const { cost_rag, scope_rag, time_rag } = response.data;
+            return { cost_rag, scope_rag, time_rag };
+        } catch (error) {
+            console.error(`Error fetching latest RAG status for project ID ${projectId}:`, error);
+            return { scope_rag: 'unknown', time_rag: 'unknown', cost_rag: 'unknown' };
+        }
+    };
+
+    const fetchProjectsAndDetails = async () => {
+        try {
+          // First, fetch all projects.
+          const projectsResponse = await axios.get('http://localhost:5001/api/projects/getall');
+          const fetchedProjects = projectsResponse.data;
+      
+          // Now, fetch additional details for each project.
+          const additionalDetailsPromises = fetchedProjects.flatMap(project => [
+            fetchPhoto(project.business_owner).then(photoUrl => ({ ['photo_' + project.business_owner]: photoUrl })),
+            fetchPhoto(project.project_manager).then(photoUrl => ({ ['photo_' + project.project_manager]: photoUrl })),
+            fetchNames(project.business_owner).then(name => ({ ['name_' + project.business_owner]: name })),
+            fetchNames(project.project_manager).then(name => ({ ['name_' + project.project_manager]: name })),
+            fetchLatestRAGStatus(project.idproject).then(ragStatus => ({ ['rag_' + project.idproject]: ragStatus }))
+          ]);
+      
+          const combinedResults = await Promise.all(additionalDetailsPromises);
+      
+          // Process combined results and update relevant states.
+          let photosObj = {}, namesObj = {}, ragStatusesObj = {};
+          combinedResults.forEach(result => {
+            const key = Object.keys(result)[0];
+            if (key.startsWith('photo_')) {
+              photosObj[key] = result[key];
+            } else if (key.startsWith('name_')) {
+              namesObj[key] = result[key];
+            } else if (key.startsWith('rag_')) {
+              ragStatusesObj[key] = result[key];
+            }
+          });
+      
+          // Update state with projects and their additional details.
+          setProjects(fetchedProjects);
+          setPhotos(photosObj);
+          setNames(namesObj);
+          setRagStatuses(ragStatusesObj);
+        } catch (error) {
+          console.error('Error fetching projects and additional details:', error);
         }
     };
 
     useEffect(() => {
-        fetchProjects();
-    }, []);
+        fetchProjectsAndDetails(); // This function will utilize the above-defined functions.
+      }, []);
 
     const handleBackClick = () => {
         navigate(-1);
@@ -53,7 +120,6 @@ const ProjectsOverview = () => {
     const handleDeleteProject = async (projectId) => {
         try {
             await axios.delete(`http://localhost:5001/api/projects/delete/${projectId}`);
-            // Filter out the deleted project from the projects state to update the UI.
             setProjects(prevProjects => prevProjects.filter(project => project.idproject !== projectId));
         } catch (error) {
             console.error('Error deleting project:', error);
@@ -80,6 +146,25 @@ const ProjectsOverview = () => {
         const options = { day: '2-digit', month: 'short', year: 'numeric' };
         return new Date(dateString).toLocaleDateString('en-GB', options);
     };
+
+    const formatBudget = (budget) => {
+        if (budget >= 1000000) {
+            return `$${(budget / 1000000).toFixed(1)}M`;
+        } else if (budget >= 1000) {
+            return `$${(budget / 1000).toFixed(1)}K`;
+        } else {
+            return `$${budget}`;
+        }
+    };
+
+    const getRAGColor = (rag) => {
+        switch (rag) {
+            case 'Red': return 'bg-red-500';
+            case 'Amber': return 'bg-yellow-500';
+            case 'Green': return 'bg-green-500';
+            default: return 'bg-gray-500';
+        }
+    }
     
     // An array of phases for demonstration purposes
     const phases = [
@@ -187,63 +272,89 @@ const ProjectsOverview = () => {
                         </div>
                     </div>
                 )}
-        
+
                 {/* Render a table for each phase */}
                 {phases.map((phase, phaseIndex) => (
-                <div key={phase} className="mb-8 w-full">
-                    <h2 className="text-xl font-bold mb-3">{t(phase)}</h2>
-                    <div className="overflow-y-auto max-h-[calc(6*4.5rem)]">
+                    <div key={phase} className="mb-8 w-full">
+                        <h2 className="text-xl font-bold mb-3">{t(phase)}</h2>
+                        <div className="overflow-y-auto max-h-[calc(6*4.5rem)]">
                         <table className="min-w-full leading-normal w-full">
-                            <thead className={`text-white ${['bg-blue-500', 'bg-green-500', 'bg-red-500', 'bg-yellow-500', 'bg-purple-500'][phaseIndex % 5]}`}>
-                                <tr>
-                                    <th className="px-5 py-3 border-b-2 border-gray-200 uppercase tracking-wider">{t("project_name")}</th>
-                                    <th className="px-5 py-3 border-b-2 border-gray-200 uppercase tracking-wider">{t("program")}</th>
-                                    <th className="px-5 py-3 border-b-2 border-gray-200 uppercase tracking-wider">{t("status")}</th>
-                                    <th className="px-5 py-3 border-b-2 border-gray-200 uppercase tracking-wider">{t("location")}</th>
-                                    <th className="px-5 py-3 border-b-2 border-gray-200 uppercase tracking-wider">{t("business_owner")}</th>
-                                    <th className="px-5 py-3 border-b-2 border-gray-200 uppercase tracking-wider">{t("project_manager")}</th>
-                                    <th className="px-5 py-3 border-b-2 border-gray-200 uppercase tracking-wider">{t("budget_approved")}</th>
-                                    <th className="px-5 py-3 border-b-2 border-gray-200 uppercase tracking-wider">{t("phase_timeline")}</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {getProjectsByPhase(phase).length > 0 ? (
-                                    getProjectsByPhase(phase).map((project, index) => (
-                                        <tr key={project.idproject} 
-                                            className={`group cursor-pointer transition duration-300 ease-in-out ${index % 2 === 0 ? 'bg-white' : 'bg-gray-50'} hover:bg-gray-200 relative`} 
-                                            onClick={() => handleRowClick(project.idproject)}
-                                        >
-                                            <td className="px-5 py-2 border-b border-gray-200 text-sm">{project.name}</td>
-                                            <td className="px-5 py-2 border-b border-gray-200 text-sm">{project.program}</td>
-                                            <td className="px-5 py-2 border-b border-gray-200 text-sm">{project.status}</td>
-                                            <td className="px-5 py-2 border-b border-gray-200 text-sm">{project.location}</td>
-                                            <td className="px-5 py-2 border-b border-gray-200 text-sm">{project.business_owner}</td>
-                                            <td className="px-5 py-2 border-b border-gray-200 text-sm">{project.project_manager}</td>
-                                            <td className="px-5 py-2 border-b border-gray-200 text-sm">{project.budget_approved}</td>
-                                            <td className="px-5 py-2 border-b border-gray-200 text-sm flex justify-between items-center">
-                                                {`${formatDate(project.phase_start)} - ${formatDate(project.phase_end)}`}
-                                                <div className="opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-in-out absolute right-0 pr-4">
-                                                    <button onClick={(e) => { e.stopPropagation(); handleDeleteProject(project.idproject); }} className="text-red-500 hover:text-red-700">
-                                                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-                                                        </svg>
-                                                    </button>
-                                                </div>
+                                <thead className={`sticky top-0 text-white ${headerColors[phaseIndex % headerColors.length]} text-left text-sm`}>
+                                    <tr>
+                                        <th className="px-5 py-3 border-b-2 border-gray-200 tracking-wider">{t('project_name')}</th>
+                                        <th className="px-5 py-3 border-b-2 border-gray-200 tracking-wider text-center">{t('status')}</th>
+                                        <th className="px-5 py-3 border-b-2 border-gray-200 tracking-wider">{t('business_owner')}</th>
+                                        <th className="px-5 py-3 border-b-2 border-gray-200 tracking-wider">{t('project_manager')}</th>
+                                        <th className="px-5 py-3 border-b-2 border-gray-200 tracking-wider">{t('phase_timeline')}</th>
+                                        <th className="px-5 py-3 border-b-2 border-gray-200 tracking-wider text-center">{t('scope')}</th>
+                                        <th className="px-5 py-3 border-b-2 border-gray-200 tracking-wider text-center">{t('time')}</th>
+                                        <th className="px-5 py-3 border-b-2 border-gray-200 tracking-wider text-center">{t('cost')}</th>
+                                        <th className="px-5 py-3 border-b-2 border-gray-200 tracking-wider">{t('budget_approved')}</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {getProjectsByPhase(phase).length > 0 ? (
+                                        getProjectsByPhase(phase).map((project, index) => {
+                                            const rag = ragStatuses[`rag_${project.idproject}`] || { scope_rag: 'unknown', time_rag: 'unknown', cost_rag: 'unknown' };  // Define 'rag' correctly here
+                                            return (
+                                                <tr key={project.idproject} 
+                                                    onClick={() => handleRowClick(project.idproject)}
+                                                    className={`group cursor-pointer transition duration-300 ease-in-out ${index % 2 === 0 ? 'bg-white' : 'bg-gray-50'} hover:bg-gray-200 relative`} 
+                                                >
+                                                    <td className="px-5 py-2 border-b border-gray-200 text-sm">{project.name}</td>
+                                                    <td className="px-5 py-2 border-b border-gray-200 text-sm text-center">
+                                                        <span className={`inline-block w-4 h-4 rounded-full ${getRAGColor(project.status)}`} />
+                                                    </td>
+                                                    <td className="px-5 py-2 border-b border-gray-200 text-sm">
+                                                        <div className="flex items-center">
+                                                            <img src={photos[`photo_${project.business_owner}`] || 'https://via.placeholder.com/150'} alt="Business Owner" className="h-7 w-7 rounded-full" />
+                                                            <span className="ml-3">{names[`name_${project.business_owner}`] || 'N/A'}</span>
+                                                        </div>
+                                                    </td>
+                                                    <td className="px-5 py-2 border-b border-gray-200 text-sm">
+                                                        <div className="flex items-center">
+                                                            <img src={photos[`photo_${project.project_manager}`] || 'https://via.placeholder.com/150'} alt="Project Manager" className="h-7 w-7 rounded-full" />
+                                                            <span className="ml-3">{names[`name_${project.project_manager}`] || 'N/A'}</span>
+                                                        </div>
+                                                    </td>
+                                                    <td className="px-5 py-2 border-b border-gray-200 text-sm">
+                                                        {`${formatDate(project.phase_start)} - ${formatDate(project.phase_end)}`}
+                                                    </td>
+                                                    <td className="px-5 py-2 border-b border-gray-200 text-sm text-center">
+                                                        <span className={`inline-block w-4 h-4 rounded-full ${getRAGColor(rag.scope_rag)}`} />
+                                                    </td>
+                                                    <td className="px-5 py-2 border-b border-gray-200 text-sm text-center">
+                                                        <span className={`inline-block w-4 h-4 rounded-full ${getRAGColor(rag.time_rag)}`} />
+                                                    </td>
+                                                    <td className="px-5 py-2 border-b border-gray-200 text-sm text-center">
+                                                        <span className={`inline-block w-4 h-4 rounded-full ${getRAGColor(rag.cost_rag)}`} />
+                                                    </td>
+                                                    <td className="px-5 py-2 border-b border-gray-200 text-sm flex justify-between items-center" style={{paddingBottom: 'calc(0.5rem + 3px)'}}>
+                                                        {formatBudget(project.budget_approved)}
+                                                        <button 
+                                                            onClick={(e) => { e.stopPropagation(); handleDeleteProject(project.idproject); }} 
+                                                            className="delete-btn ml-4 text-red-500 hover:text-red-700 opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-in-out"
+                                                        >
+                                                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                                                                <path strokeLinecap="round" strokeLinejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                                                            </svg>
+                                                        </button>
+                                                    </td>
+                                                </tr>
+                                            );
+                                        })
+                                    ) : (
+                                        <tr>
+                                            <td colSpan="9" className="text-center px-5 py-2 border-b border-gray-200 text-lg font-bold">
+                                                {t("no_projects_in_phase")}
                                             </td>
                                         </tr>
-                                    ))
-                                ) : (
-                                    <tr>
-                                        <td colSpan="8" className="text-center px-5 py-5 border-b border-gray-200 text-sm">
-                                            {t("no_projects_in_phase")}
-                                        </td>
-                                    </tr>
-                                )}
-                            </tbody>
-                        </table>
+                                    )}
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
-                </div>
-            ))}
+                ))}
             </div>
         </>
     );
