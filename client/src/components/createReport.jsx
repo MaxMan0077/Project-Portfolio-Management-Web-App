43167ec240ea29b4f3eb61ab75399abf1efc7410
Max Ward <maxward3@outlook.com>
2024-04-18 00:24:08 +0100

    Report validation

--- client/src/components/createReport.jsx
+++ client/src/components/createReport.jsx
@@ -16,21 +16,27 @@ const CreateStatusReportForm = () => {
     revisedStart: '',
     revisedEnd: '',
   });
+  const [errors, setErrors] = useState({});
 
   const { projectId } = useParams();
   const navigate = useNavigate();
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
-    setStatusReport((prevStatusReport) => ({
-      ...prevStatusReport,
-      [name]: value,
-    }));
+    setStatusReport(prev => ({ ...prev, [name]: value }));
   };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
   
+    const newErrors = validateAllInputs();
+    if (Object.keys(newErrors).length > 0) {
+      setErrors(newErrors);  // Update the errors state with new errors
+      console.error('Validation errors:', newErrors);
+      return;  // Prevent form submission if errors exist
+    }
+  
+    // Form data is valid here, so proceed with the API call
     const statusReportData = {
       ...statusReport,
       project: projectId
@@ -38,18 +44,57 @@ const CreateStatusReportForm = () => {
   
     try {
       const response = await axios.post(`http://localhost:5001/api/reports/${projectId}`, statusReportData);
-      // Handle success response
-      navigate(`/project-details/${projectId}`); // Navigate to the project details page
+      navigate(`/project-details/${projectId}`);  // Navigate to the project details page
     } catch (error) {
       console.error('Error submitting status report:', error);
-      // Handle error response
     }
-  };
+  };  
+  
+  const validateAllInputs = () => {
+    let newErrors = {};
+  
+    // Check the date field
+    if (!statusReport.date) {
+      newErrors.date = 'Date is required.';
+    }
+  
+    // Check RAG status fields
+    ['scopeRag', 'timeRag', 'costRag'].forEach(field => {
+      if (!statusReport[field]) {
+        let fieldName = field.replace('Rag', ' RAG'); // Prepare field name for display
+        fieldName = fieldName.charAt(0).toUpperCase() + fieldName.slice(1);
+        newErrors[field] = `${fieldName} is required.`;
+      }
+    });
+  
+    // Check the percentage field
+    if (!statusReport.percentage) {
+      newErrors.percentage = 'Percentage Complete is required.';
+    } else if (isNaN(statusReport.percentage) || parseInt(statusReport.percentage) < 0 || parseInt(statusReport.percentage) > 100) {
+      newErrors.percentage = 'Percentage must be a number between 0 and 100.';
+    }
+  
+    // Check the start and end dates for not being empty and validation
+    if (!statusReport.revisedStart) {
+      newErrors.revisedStart = 'Start date is required.';
+    }
+  
+    if (!statusReport.revisedEnd) {
+      newErrors.revisedEnd = 'End date is required.';
+    } else if (statusReport.revisedStart && new Date(statusReport.revisedStart) >= new Date(statusReport.revisedEnd)) {
+      newErrors.revisedEnd = 'End date must be after the start date.';
+    }
+  
+    return newErrors;
+  };  
 
   const handleCancel = () => {
     navigate(`/project-details/${projectId}`);
   };
-  
+
+  // Dynamically set input class based on errors
+  const inputClass = (key) => `shadow border ${errors[key] ? 'border-red-500' : 'border-gray-300'} rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline`;
+
   return (
     <>
       <Navbar />
@@ -57,93 +102,87 @@ const CreateStatusReportForm = () => {
         <form onSubmit={handleSubmit} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
           <h2 className="text-2xl font-bold text-gray-700 mb-4">{t('create_status_report_for_project')} {projectId}</h2>
           
-          {/* First row of inputs including the date */}
-          <div className="flex mb-4">
-            {/* Date Field */}
-            <div className="w-1/3 mr-2">
-              <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="date">
-                {t('date')}
-              </label>
-              <input
-                className="shadow border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                id="date"
-                name="date"
-                type="date"
-                value={statusReport.date}
-                onChange={handleInputChange}
-              />
-            </div>
-  
-            {/* Placeholder for alignment */}
-            <div className="w-1/3 mx-2"></div>
-            <div className="w-1/3 ml-2"></div>
+          {/* Date Field */}
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="date">
+              {t('date')}
+            </label>
+            <input
+              className={inputClass('date')}
+              id="date"
+              name="date"
+              type="date"
+              value={statusReport.date}
+              onChange={handleInputChange}
+            />
+            {errors.date && <p className="text-red-500 text-xs italic">{errors.date}</p>}
           </div>
-  
+
           {/* RAG Status Fields */}
           <div className="flex mb-4">
-            {/* Scope RAG */}
             <div className="w-1/3 mr-2">
               <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="scopeRag">
                 {t('scope_rag')}
               </label>
               <select
+                className={inputClass('scopeRag')}
                 name="scopeRag"
-                value={statusReport.scopeRag}
                 onChange={handleInputChange}
-                className="shadow border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                value={statusReport.scopeRag}
               >
                 <option value="">{t('select_scope_rag_status')}</option>
                 <option value="Red">{t('red')}</option>
                 <option value="Amber">{t('amber')}</option>
                 <option value="Green">{t('green')}</option>
               </select>
+              {errors.scopeRag && <p className="text-red-500 text-xs italic">{errors.scopeRag}</p>}
             </div>
-  
-            {/* Time RAG */}
-            <div className="w-1/3 mr-2">
+
+            <div className="w-1/3 mx-2">
               <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="timeRag">
                 {t('time_rag')}
               </label>
               <select
+                className={inputClass('timeRag')}
                 name="timeRag"
-                value={statusReport.timeRag}
                 onChange={handleInputChange}
-                className="shadow border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                value={statusReport.timeRag}
               >
                 <option value="">{t('select_time_rag_status')}</option>
                 <option value="Red">{t('red')}</option>
                 <option value="Amber">{t('amber')}</option>
                 <option value="Green">{t('green')}</option>
               </select>
+              {errors.timeRag && <p className="text-red-500 text-xs italic">{errors.timeRag}</p>}
             </div>
-  
-            {/* Cost RAG */}
-            <div className="w-1/3 mr-2">
+
+            <div className="w-1/3 ml-2">
               <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="costRag">
                 {t('cost_rag')}
               </label>
               <select
+                className={inputClass('costRag')}
                 name="costRag"
-                value={statusReport.costRag}
                 onChange={handleInputChange}
-                className="shadow border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                value={statusReport.costRag}
               >
                 <option value="">{t('select_cost_rag_status')}</option>
                 <option value="Red">{t('red')}</option>
                 <option value="Amber">{t('amber')}</option>
                 <option value="Green">{t('green')}</option>
               </select>
+              {errors.costRag && <p className="text-red-500 text-xs italic">{errors.costRag}</p>}
             </div>
           </div>
-  
-          {/* Second Line: Phase % Complete and Revised Phase Start/End Dates */}
+
+          {/* Percentage Complete and Revised Phase Start/End Dates */}
           <div className="flex mb-4">
             <div className="w-1/3 mr-2">
               <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="percentage">
                 {t('phase_percentage_complete')}
               </label>
               <input
-                className="shadow border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                className={inputClass('percentage')}
                 id="percentage"
                 name="percentage"
                 type="text"
@@ -151,35 +190,40 @@ const CreateStatusReportForm = () => {
                 value={statusReport.percentage}
                 onChange={handleInputChange}
               />
+              {errors.percentage && <p className="text-red-500 text-xs italic">{errors.percentage}</p>}
             </div>
+
             <div className="w-1/3 mx-2">
               <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="revisedStart">
                 {t('revised_phase_start_date')}
               </label>
               <input
-                className="shadow border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                className={inputClass('revisedStart')}
                 id="revisedStart"
                 name="revisedStart"
                 type="date"
                 value={statusReport.revisedStart}
                 onChange={handleInputChange}
               />
+              {errors.revisedStart && <p className="text-red-500 text-xs italic">{errors.revisedStart}</p>}
             </div>
+
             <div className="w-1/3 ml-2">
               <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="revisedEnd">
                 {t('revised_phase_end_date')}
               </label>
               <input
-                className="shadow border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                className={inputClass('revisedEnd')}
                 id="revisedEnd"
                 name="revisedEnd"
                 type="date"
                 value={statusReport.revisedEnd}
                 onChange={handleInputChange}
               />
+              {errors.revisedEnd && <p className="text-red-500 text-xs italic">{errors.revisedEnd}</p>}
             </div>
           </div>
-  
+
           {/* Submit and Cancel Buttons */}
           <div className="flex items-center justify-between mt-4">
             <button
@@ -199,7 +243,6 @@ const CreateStatusReportForm = () => {
         </form>
       </div>
     </>
-  );  
+  );
 };
-
 export default CreateStatusReportForm;
