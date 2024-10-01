ae7a43ea77c2f21baa0d76378ec221fd19009aff
Max Ward <116183252+MaxMan0077@users.noreply.github.com>
2024-10-01 15:17:39 +0100

    Update main.js

--- server/routes/main.js
+++ server/routes/main.js
@@ -3,13 +3,11 @@ const router = express.Router();
 
 // Define generic routes
 router.get('/', (req, res) => {
-    // Logic to handle GET request for all projects
+    
 });
 
 router.post('/', (req, res) => {
-    // Logic to handle POST request to add a new project
+    
 });
-
-// More project-related routes (PUT, DELETE, etc.)
 
 module.exports = router;
